b9e4726dd72e92cceb22c84e53ad64af14a8d73b
Jonas <jetschni@gmail.com>
2016-02-11 21:11:32 +0100

    * Images updated * Minor fixes

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -10,7 +10,7 @@ var studysearchApp = angular.module('studysearchApp', ['ngMaterial', 'chart.js',
  */
 studysearchApp.constant("studysearchConfig", {
     // SPARQL Endpoint URL
-    "sparqlEndpoint": "http://fbwsvcdev.fh-brandenburg.de:8080/fuseki/EduGraph/query",
+    "sparqlEndpoint": "http://fbwsvcdev.fh-brandenburg.de:8080/fuseki/EduGraphTest/query",
     // SPARQL Queries cachen um Ladezeiten zu verringern
     "cacheSPARQLQueries": false,
     // Grenzwert damit Säule als Schwerpunkt gilt
